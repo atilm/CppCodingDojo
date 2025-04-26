c7724c83b2a9d4fa8d700f52417bf05bee812e26
Andreas Thielmann <thielmann.andreas@googlemail.com>
2025-04-26 20:49:20 +0000

    Auto-commit

--- src/tests.cpp
+++ src/tests.cpp
@@ -10,13 +10,25 @@ TEST_CASE("parsing an empty string returns an empty vector") {
     REQUIRE(result.Numbers == expected);
 }
 
-TEST_CASE("parsing a string with one number returns a vector with that number") {  
+TEST_CASE("parsing a string with all eights can be parsed") {  
     std::string input = ""
     " _  _  _  _  _  _  _  _  _ \n"
     "|_||_||_||_||_||_||_||_||_|\n"
     "|_||_||_||_||_||_||_||_||_|\n"
     "                           \n";
     std::vector<std::string> expected = {"888888888"};
+    auto result = parse(input);
+    REQUIRE(result.Status == ErrorCode::SUCCESS);
+    REQUIRE(result.Numbers == expected);
+}
+
+TEST_CASE("parsing a string with all digits can be parsed") {  
+    std::string input = ""
+    "    _  _     _  _  _  _  _ \n"
+    "  | _| _||_||_ |_   ||_||_|\n"
+    "  ||_  _|  | _||_|  ||_| _|\n"
+    "                           \n";
+    std::vector<std::string> expected = {"123456789"};
     auto result = parse(input);
     REQUIRE(result.Status == ErrorCode::SUCCESS);
     REQUIRE(result.Numbers == expected);
