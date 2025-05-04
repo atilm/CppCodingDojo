955674a80ba27610a529175013eb3c425a5b6c1d
Andreas Thielmann <thielmann.andreas@googlemail.com>
2025-05-04 17:59:53 +0000

    fix tests.cpp

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,5 +1,4 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
+#include <catch2/catch_all.hpp>
 #include "lib.hpp"
 
 TEST_CASE("parsing an empty string returns an empty vector") {  
